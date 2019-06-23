33b3a73cdac5ac02b0383b0b9bdbb116499e9579
asaf <asaf@ddaylabs.com>
2019-06-23 13:40:29 +0300

    router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Router, Link } from "@reach/router"
+
+import NationSelection from './routes/NationSelection'
+import Order from './routes/Order'
+import Output from './routes/Output'
+import Setup from './routes/Setup'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <NationSelection path="/" />
+        <Setup path="setup" />
+        <Order path="order" />
+        <Output path="output" />
+      </Router>
+      <div className="footer">
+        <nav>
+          <Link to="/">Home</Link> |{" "}
+          <Link to="setup">Setup </Link> |{" "}
+          <Link to="order">Order</Link> |{" "}
+          <Link to="output">Output</Link>
+        </nav>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
