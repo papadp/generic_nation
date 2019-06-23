6cbc175c8fc84b472ae39cbc15c7937c223541de
dronen <rdavid415@gmail.com>
2019-06-23 18:09:47 +0300

    nation links to order and setup

--- frontend/src/routes/NationSelection/NationSelectionButton.js
+++ frontend/src/routes/NationSelection/NationSelectionButton.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import {Link} from "@reach/router";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faEyeDropper} from "@fortawesome/free-solid-svg-icons";
 
 export default ({nation}) => {
     return (
-        <div className="nation-button nation-selection-button">
-            <h1 className="nation-name"> {nation.name}  </h1>
+        <button className="nation-button nation-selection-button">
+            <Link className="nation-order-link" to={`/order/${nation.id}`}>
+                <h2 className="nation-name"> {nation.name}  </h2>
+            </Link>
 
-        </div>
+            <div className="edit-nation-icon" title="Edit Nation">
+                <Link to={`/setup/${nation.id}`}>
+                    <FontAwesomeIcon icon={faEyeDropper}/>
+                </Link>
+            </div>
+
+        </button>
     )
 }
