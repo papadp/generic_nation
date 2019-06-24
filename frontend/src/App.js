bf2ff9e2a9f48ab442506b3dd1bcdc98a2ce4787
asaf <asaf@ddaylabs.com>
2019-06-24 13:18:13 +0300

    user selection

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.scss';
 import { Router, Link } from "@reach/router"
 
@@ -9,11 +8,24 @@ import Output from './routes/Output'
 import Setup from './routes/Setup'
 
 function App() {
+    const [user, setUser] = useState('El Hamin')
   return (
     <div className="App">
-        <Link to={"/"} >
-            <header>GENERIC NATION</header>
-        </Link>
+        <header>
+            <Link to={"/"} >GENERIC NATION</Link>
+            <div className="user">
+                <select value={user} onChange={e => setUser(e.target.value)}>
+                    <option value="El Hamin">El Hamin</option>
+                    <option value="Adimor">Adimor</option>
+                    <option value="Dima">Dima</option>
+                    <option value="Vicky">Vicky</option>
+                    <option value="Eden">Eden</option>
+                    <option value="Dodro">Dodro</option>
+                    <option value="Itay">Itay</option>
+                    <option value="Asaf">Asaf</option>
+                </select>
+            </div>
+        </header>
       <Router>
         <NationSelection path="/" />
         <Setup path="setup/:nationId" />
